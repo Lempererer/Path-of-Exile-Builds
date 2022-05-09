fa7b60a7fcde0693afe2dec0926514ab38ec624d
Tony Liang <19316@burnside.school.nz>
2022-05-10 09:13:50 +1200

    added lots of code comments

--- venv/poe_builds_flask.py
+++ venv/poe_builds_flask.py
@@ -3,23 +3,28 @@ import sqlite3
 
 app = Flask(__name__)
 
+#the database
 DATABASE = "poe_builds_database.db"
 
+#connecting to database
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+#disconnecting to database
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
+#home page
 @app.route("/")
 def home():
     return render_template("contents.html")
 
+#runs flask in terminal
 if __name__ == "__main__":
     app.run(debug=True)
