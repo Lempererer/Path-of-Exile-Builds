fd8f9ac66fd41e0ccc40e783d88b71322be9b247
Tony Liang <19316@burnside.school.nz>
2022-06-09 14:14:29 +1200

    finished basic design | navigation bar on website.

--- venv/poe_builds_flask.py
+++ venv/poe_builds_flask.py
@@ -1,10 +1,10 @@
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, redirect, request
 import sqlite3
 
 app = Flask(__name__)
 
-#the database
-DATABASE = "poe_builds_database.db"
+#establishing database route
+DATABASE = "venv\poe_builds_database.db"
 
 #connecting to database
 def get_db():
@@ -23,7 +23,21 @@ def close_connection(exception):
 #home page
 @app.route("/")
 def home():
-    return render_template("contents.html")
+    return render_template("index.html")
+
+#builds page
+@app.route("/builds")
+def builds():
+    return render_template("builds.html")
+
+#page for build
+@app.route("/build__impale-crit_cyclone_slayer")
+def build():
+    cursor = get_db().cursor()
+    sql = "SELECT * FROM items"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return render_template("build__impale-crit_cyclone_slayer.html", results = results)
 
 #runs flask in terminal
 if __name__ == "__main__":
