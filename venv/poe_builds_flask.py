2051661f7ef69d6c3411f9770e02da11067da3e4
Tony Liang <19316@burnside.school.nz>
2022-11-02 09:41:29 +1300

    can view individual items and create/delete builds

--- venv/poe_builds_flask.py
+++ venv/poe_builds_flask.py
@@ -1,3 +1,4 @@
+from unittest import result
 from flask import Flask, g, render_template, redirect, request, url_for
 import sqlite3
 
@@ -29,58 +30,237 @@ def home():
 @app.route("/builds")
 def builds():
     cursor = get_db().cursor()
-    sql = "SELECT build_name FROM builds"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return render_template("builds.html", results = results)
 
-#impale cyclone page
-@app.route("/impale_cyclone")
-def impale_cyclone():
+    build_name_sql = '''
+    SELECT build_name
+    FROM builds
+    '''
+    cursor.execute(build_name_sql)
+    build_name_results = cursor.fetchall()
+
+    amulet_sql = '''
+    SELECT item_name, item_id
+    FROM items
+    WHERE item_type = ?
+    '''
+    cursor.execute(amulet_sql, ("amulet", ))
+    amulet_results = cursor.fetchall()
+
+    belt_sql = '''
+    SELECT item_name, item_id
+    FROM items
+    WHERE item_type = ?
+    '''
+    cursor.execute(belt_sql, ("belt", ))
+    belt_results = cursor.fetchall()
+
+    body_armour_sql = '''
+    SELECT item_name, item_id
+    FROM items
+    WHERE item_type = ?
+    '''
+    cursor.execute(body_armour_sql, ("body_armour", ))
+    body_armour_results = cursor.fetchall()
+
+    boots_sql = '''
+    SELECT item_name, item_id
+    FROM items
+    WHERE item_type = ?
+    '''
+    cursor.execute(boots_sql, ("boots", ))
+    boots_results = cursor.fetchall()
+
+    gloves_sql = '''
+    SELECT item_name, item_id
+    FROM items
+    WHERE item_type = ?
+    '''
+    cursor.execute(gloves_sql, ("gloves", ))
+    gloves_results = cursor.fetchall()
+
+    hand_sql = '''
+    SELECT item_name, item_id
+    FROM items
+    WHERE item_type = ?
+    '''
+    cursor.execute(hand_sql, ("hand", ))
+    hand_results = cursor.fetchall()
+
+    helmet_sql = '''
+    SELECT item_name, item_id
+    FROM items
+    WHERE item_type = ?
+    '''
+    cursor.execute(helmet_sql, ("helmet", ))
+    helmet_results = cursor.fetchall()
+
+    ring_sql = '''
+    SELECT item_name, item_id
+    FROM items
+    WHERE item_type = ?
+    '''
+    cursor.execute(ring_sql, ("ring", ))
+    ring_results = cursor.fetchall()
+
+
+    print(build_name_results)
+    print(amulet_results)
+
+    return render_template("builds.html", build_name_results = build_name_results, amulet_results = amulet_results, belt_results = belt_results, body_armour_results = body_armour_results, boots_results = boots_results, gloves_results = gloves_results, hand_results = hand_results, helmet_results = helmet_results, ring_results = ring_results)
+
+#build page
+@app.route("/builds/<string:build_name>")
+def build(build_name):
+    cursor = get_db().cursor()
+
+    build_name_sql = '''
+    SELECT build_name
+    FROM builds
+    WHERE build_name=?
+    '''
+    cursor.execute(build_name_sql, (build_name, ))
+    build_name_results = cursor.fetchall()
+    print(build_name_results)
+
+    amulet_sql = '''
+    SELECT items.item_img, items.item_id
+    FROM builds
+    JOIN items ON builds.amulet_id = items.item_id
+    WHERE build_name= ? 
+    '''
+    cursor.execute(amulet_sql, (build_name, ))
+    amulet_results = cursor.fetchall()
+    print(amulet_results)
+
+    belt_sql = '''
+    SELECT items.item_img, items.item_id
+    FROM builds
+    JOIN items ON builds.belt_id = items.item_id
+    WHERE build_name= ? 
+    '''
+    cursor.execute(belt_sql, (build_name, ))
+    belt_results = cursor.fetchall()
+
+    body_armour_sql = '''
+    SELECT items.item_img, items.item_id
+    FROM builds
+    JOIN items ON builds.body_armour_id = items.item_id
+    WHERE build_name= ? 
+    '''
+    cursor.execute(body_armour_sql, (build_name, ))
+    body_armour_results = cursor.fetchall()
+
+    boots_sql = '''
+    SELECT items.item_img, items.item_id
+    FROM builds
+    JOIN items ON builds.boots_id = items.item_id
+    WHERE build_name= ?
+    '''
+    cursor.execute(boots_sql, (build_name, ))
+    boots_results = cursor.fetchall()
+
+    gloves_sql = '''
+    SELECT items.item_img, items.item_id
+    FROM builds
+    JOIN items ON builds.gloves_id = items.item_id
+    WHERE build_name= ? 
+    '''
+    cursor.execute(gloves_sql, (build_name, ))
+    gloves_results = cursor.fetchall()
+
+    main_hand_sql = '''
+    SELECT items.item_img, items.item_id
+    FROM builds
+    JOIN items ON builds.main_hand_id = items.item_id
+    WHERE build_name= ? 
+    '''
+    cursor.execute(main_hand_sql, (build_name, ))
+    main_hand_results = cursor.fetchall()
+
+    off_hand_sql = '''
+    SELECT items.item_img, items.item_id
+    FROM builds
+    JOIN items ON builds.off_hand_id = items.item_id
+    WHERE build_name= ? 
+    '''
+    cursor.execute(off_hand_sql, (build_name, ))
+    off_hand_results = cursor.fetchall()
+
+    helmet_sql = '''
+    SELECT items.item_img, items.item_id
+    FROM builds
+    JOIN items ON builds.helmet_id = items.item_id
+    WHERE build_name= ? 
+    '''
+    cursor.execute(helmet_sql, (build_name, ))
+    helmet_results = cursor.fetchall()
+
+    ring_1_sql = '''
+    SELECT items.item_img, items.item_id
+    FROM builds
+    JOIN items ON builds.ring_1_id = items.item_id
+    WHERE build_name= ? 
+    '''
+    cursor.execute(ring_1_sql, (build_name, ))
+    ring_1_results = cursor.fetchall()
+
+    ring_2_sql = '''
+    SELECT items.item_img, items.item_id
+    FROM builds
+    JOIN items ON builds.ring_2_id = items.item_id
+    WHERE build_name= ? 
+    '''
+    cursor.execute(ring_2_sql, (build_name, ))
+    ring_2_results = cursor.fetchall()
+    
+    return render_template("build.html", build_name_results = build_name_results, amulet_results = amulet_results, belt_results = belt_results, body_armour_results = body_armour_results, boots_results = boots_results, gloves_results = gloves_results, main_hand_results = main_hand_results, off_hand_results = off_hand_results, helmet_results = helmet_results, ring_1_results = ring_1_results, ring_2_results = ring_2_results)
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    print ("adding data")
+    if request.method == "POST":
+        cursor = get_db().cursor()
+        new_build_name = request.form["builds_build_name_id"]
+        new_body_armour_id = request.form["select_body_armour"]
+        new_helmet_id = request.form["select_helmet"]
+        new_amulet_id = request.form["select_amulet"]
+        new_gloves_id = request.form["select_gloves"]
+        new_boots_id = request.form["select_boots"]
+        new_ring_1_id = request.form["select_ring_1"]
+        new_ring_2_id = request.form["select_ring_2"]
+        new_main_hand_id = request.form["select_main_hand"]
+        new_off_hand_id = request.form["select_off_hand"]
+        new_belt_id = request.form["select_belt"]
+        add_sql = "INSERT INTO builds(build_name, body_armour_id, helmet_id, amulet_id, gloves_id, boots_id, ring_1_id, ring_2_id, main_hand_id, off_hand_id, belt_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        cursor.execute(add_sql,(new_build_name, new_body_armour_id, new_helmet_id, new_amulet_id, new_gloves_id, new_boots_id, new_ring_1_id, new_ring_2_id, new_main_hand_id, new_off_hand_id, new_belt_id))
+        get_db().commit()
+    return redirect("/builds")
+
+@app.route('/delete', methods=["GET", "POST"])
+def delete():
+    if request.method == "POST":
+        cursor = get_db().cursor()
+        build_name= str(request.form["delete_builds_build_name_id"])
+        sql = '''
+        DELETE FROM builds 
+        WHERE build_name = ?
+        '''
+        cursor.execute(sql,(build_name, ))
+        get_db().commit()
+    return redirect("/builds")
+
+@app.route("/item/<int:item_id>")
+def item_description(item_id):
     cursor = get_db().cursor()
     sql = '''
-    SELECT build_name, amulet.amulet_img, belt.belt_img, body_armour.body_armour_img, boots.boots_img, gloves.gloves_img, helmet.helmet_img, main_hand.main_hand_img, off_hand.off_hand_img, ring_1.ring_img, ring_2.ring_img, 
-    amulet.amulet_data_img, belt.belt_data_img, body_armour.body_armour_data_img, boots.boots_data_img, gloves.gloves_data_img, helmet.helmet_data_img, main_hand.main_hand_data_img, off_hand.off_hand_data_img, ring_1.ring_data_img, ring_2.ring_data_img
-    FROM builds
-    JOIN amulet ON builds.amulet_id = amulet.amulet_id
-    JOIN belt ON builds.belt_id = belt.belt_id
-    JOIN body_armour ON builds.body_armour_id = body_armour.body_armour_id
-    JOIN boots ON builds.boots_id = boots.boots_id
-    JOIN gloves ON builds.gloves_id = gloves.gloves_id
-    JOIN helmet ON builds.helmet_id = helmet.helmet_id
-    JOIN main_hand ON builds.main_hand_id = main_hand.main_hand_id 
-    JOIN off_hand ON builds.off_hand_id = off_hand.off_hand_id
-    JOIN ring_1 ON builds.ring_1_id = ring_1.ring_id
-    JOIN ring_2 ON builds.ring_2_id = ring_2.ring_id
-    WHERE build_name='Impale Cyclone';
+    SELECT item_data_img
+    FROM items
+    WHERE item_id = ?
     '''
-    cursor.execute(sql)
+    cursor.execute(sql, (item_id, ))
     results = cursor.fetchall()
-    return render_template("impale_cyclone.html", results = results)
-
-#righteous fire/fire trap page
-@app.route("/righteous_fire")
-def righteous_fire():
-    cursor = get_db().cursor()
-    sql = '''
-    SELECT build_name, amulet.amulet_img, belt.belt_img, body_armour.body_armour_img, boots.boots_img, gloves.gloves_img, helmet.helmet_img, main_hand.main_hand_img, off_hand.off_hand_img, ring_1.ring_img, ring_2.ring_img, 
-    amulet.amulet_data_img, belt.belt_data_img, body_armour.body_armour_data_img, boots.boots_data_img, gloves.gloves_data_img, helmet.helmet_data_img, main_hand.main_hand_data_img, off_hand.off_hand_data_img, ring_1.ring_data_img, ring_2.ring_data_img
-    FROM builds
-    JOIN amulet ON builds.amulet_id = amulet.amulet_id
-    JOIN belt ON builds.belt_id = belt.belt_id
-    JOIN body_armour ON builds.body_armour_id = body_armour.body_armour_id
-    JOIN boots ON builds.boots_id = boots.boots_id
-    JOIN gloves ON builds.gloves_id = gloves.gloves_id
-    JOIN helmet ON builds.helmet_id = helmet.helmet_id
-    JOIN main_hand ON builds.main_hand_id = main_hand.main_hand_id 
-    JOIN off_hand ON builds.off_hand_id = off_hand.off_hand_id
-    JOIN ring_1 ON builds.ring_1_id = ring_1.ring_id
-    JOIN ring_2 ON builds.ring_2_id = ring_2.ring_id
-    WHERE build_name='Righteous Fire/Fire Trap';
-    '''
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return render_template("righteous_fire.html", results = results)
+    print(results)
+    return render_template("item_description.html", results = results)
 
 #runs flask in terminal
 if __name__ == "__main__":
--- venv/templates/scripts.js
+++
@@ -1,3 +0,0 @@
-function open_amulet_data_img() {
-    document.getElementById("amulet_data_img").style.width = "250";
-}
